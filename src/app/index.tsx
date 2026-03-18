903424460e4d072aa4fe53bd019068069a85c293
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-03-18 09:01:50 -0300

    refactor: replace Index component with Redirect to login page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,5 @@
-import { router } from "expo-router";
-import { Text, TouchableOpacity } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <SafeAreaView>
-      <Text>Hello World</Text>
-      <TouchableOpacity onPress={() => router.push("/(private)/game")}>
-        <Text>game</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
+  return <Redirect href={"/(public)/login"} />;
 }
