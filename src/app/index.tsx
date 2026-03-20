596e6f6464585e21145b3beca1c88f3719354e03
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-03-20 11:35:24 -0300

    feat: add user authentication redirection in index component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,13 @@
+import { useAuth } from "@/services";
 import { Redirect } from "expo-router";
 
 export default function Index() {
+
+  const { user } = useAuth();
+
+  if (user) {
+    return <Redirect href={"/(private)/home"} />;
+  }
+
   return <Redirect href={"/(public)/login"} />;
 }
