a4f780ae1fd190650083058f8d2b3fd7991ec2c0
fabiano-bragaaa <fabianobragadev@gmail.com>
2026-03-26 10:47:35 -0300

    refactor: simplify Home component by removing unused authentication and difficulty state management

--- src/app/(private)/home.tsx
+++ src/app/(private)/home.tsx
@@ -1,22 +1,11 @@
 import { Page } from "@/components";
 import { DifficultySelector, HomeHeader } from "@/containers";
-import { useAuthServices } from "@/services";
-import { router } from "expo-router";
-import { useDifficulty } from "./_hooks/use-difficulty";
 
 export default function Home() {
-  const { logout } = useAuthServices();
-  const { difficulties, selectedDifficulty, setSelectedDifficulty } = useDifficulty();
-
-  function handleLogout() {
-    logout();
-    router.replace("/(public)/login");
-  }
-
   return (
     <Page withPadding>
       <HomeHeader />
-      <DifficultySelector difficulties={difficulties} selectedDifficulty={selectedDifficulty} setSelectedDifficulty={setSelectedDifficulty} />
+      <DifficultySelector />
     </Page>
   );
 }
